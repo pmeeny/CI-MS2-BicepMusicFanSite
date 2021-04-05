1f51163f7435c00dd7d2483a21a6b08754edfc27
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-04-05 22:55:51 +0100

    feat: Initial structure and content of quiz

---
+++ assets/js/quiz.js
@@ -0,0 +1,26 @@
+// https://www.codingnepalweb.com/2020/10/create-quiz-app-with-timer-using-javascript.html
+
+var questions = [
+    {
+    number: 1,
+    question: "Where are BICEP from?",
+    correctAnswer: "Belfast",
+    options: [
+      "Dublin",
+      "London",
+      "Belfast",
+      "Edinburgh"
+    ]
+  },
+    {
+    number: 2,
+    question: "What was BICEPs first album called?",
+    answer: "Bicep",
+    options: [
+      "Bicep",
+      "Nova",
+      "The light",
+      "Forever"
+    ]
+  }
+];

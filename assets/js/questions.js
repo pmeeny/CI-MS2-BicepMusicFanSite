59532adfd03c4e610593f202c2f68ab97097743d
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-04-07 10:42:44 +0100

    feat: Questions are moved to a new script

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+var questions = [
+    {
+        question: "Where are BICEP from?",
+        a: "Dublin",
+        b: "London",
+        c: "Belfast",
+        d: "Edinburgh",
+        answer: "c"
+      },
+    {
+        question: "What was BICEPs first album called?",
+        a: "Bicep",
+        b: "Nova",
+        c: "The light",
+        d: "Forever",
+        answer: "a"
+      },
+    ];
+  

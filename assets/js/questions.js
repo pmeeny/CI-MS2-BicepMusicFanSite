067da105b6fe0b60e00322829e5c55db52f3cb00
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-04-08 00:49:13 +0100

    feat: Add 2 questions for increased testing

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,19 +1,35 @@
 var questions = [
     {
-        question: "Where are BICEP from?",
-        a: "Dublin",
-        b: "London",
-        c: "Belfast",
-        d: "Edinburgh",
-        answer: "Belfast"
-      },
+      question: "Where are BICEP from?",
+      a: "Dublin",
+      b: "London",
+      c: "Belfast",
+      d: "Edinburgh",
+      answer: "Belfast"
+    },
     {
-        question: "What was BICEPs first album called?",
-        a: "Bicep",
-        b: "Nova",
-        c: "The light",
-        d: "Forever",
-        answer: "Bicep"
-      },
-    ];
+      question: "What was BICEPs first album called?",
+      a: "Bicep",
+      b: "Nova",
+      c: "The light",
+      d: "Forever",
+      answer: "Bicep"
+    },
+    {
+      question: "What year were BICEP formed?",
+      a: "2011",
+      b: "2010",
+      c: "2006",
+      d: "2009",
+      answer: "2009"
+    },
+    {
+      question: "What year did they first perform at the AVA festival",
+      a: "2015",
+      b: "2018",
+      c: "2016",
+      d: "2013",
+      answer: "2015"
+    }
+];
   

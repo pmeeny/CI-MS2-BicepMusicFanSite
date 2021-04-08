e6c0d113368ecef535d1e28f6bf55fe161b28074
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-04-08 01:14:09 +0100

    feat: New function getAlbums and displayAlbum to display album information on the page

--- assets/js/about.js
+++ assets/js/about.js
@@ -16,7 +16,7 @@ var getToken = async () => {
         return data;
     }
 
-var getAlbum = async (token) => {
+var getTopTracks = async (token) => {
         const result = await fetch(`https://api.spotify.com/v1/artists/73A3bLnfnz5BoQjb4gNCga/top-tracks?market=IE`, {
         method: 'GET',
             headers: { 'Authorization' : 'Bearer ' + token}
@@ -24,15 +24,35 @@ var getAlbum = async (token) => {
         });
         const data = await result.json();
         return data;
-    }
+}
+
+
+var getAlbums = async (token) => {
+    const result = await fetch(`https://api.spotify.com/v1/albums/4psDRFbIlUM1KUb1omccXo?market=IE`, {
+    method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+
+    });
+    const data = await result.json();
+    return data;
+}
+
 
 getToken().then(returnData =>{
-    getAlbum(returnData.access_token).then(newData=> {
+    getAlbums(returnData.access_token).then(newData=> {
         console.log(newData);
-        displayTopTracks(newData);
+        //displayTopTracks(newData);
+        displayAlbum(newData)
         console.log(newData);
     })
 });
+
+function displayAlbum(data){
+    //var img1 = new Image();
+    //img1.setAttribute("src",data.images[0]);
+    var img= '<img src ="' +data.images[0].url + '" />';
+    $("#album1").append(img)
+}
 
 function displayTopTracks(data){
     var topTracks = data.tracks;

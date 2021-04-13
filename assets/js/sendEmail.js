c55afec3c25bbdb7396a3ae03ef428bf30ae419b
Paul Meeneghan <paulmeeneghan@gmail.com>
2021-04-13 23:05:44 +0100

    feat: Send email using emailJS on mailing list link on index.html

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,20 @@
+/**
+ * [sendEmail to send email using EmailJS
+ * Credit: Code Institute material "Sending Emails Using EmailJS"]
+ * @param  contactForm [The contact form object]
+ */
+function sendMail(contactForm) {
+    emailjs.send("gmail", "bicep", {
+        "from_name": contactForm.full_name.value,
+        "from_email": contactForm.email_address.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
